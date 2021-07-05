30ab477c43f0b8f8754818108ed9b128d4df4933
Minaev Vlad <vminaev.exceedteam@gmail.com>
2021-07-06 01:48:10 +0300

    change +/- functions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import './App.css';
-import Total from './Total.js';
-import Plus from './Plus.js';
-import Minus from './Minus.js';
+import "./App.css";
+import Total from "./Total.js";
+import Plus from "./Plus.js";
+import Minus from "./Minus.js";
 
 function App(props) {
   let [total, setTotal] = useState(0);
 
   const plusFn = (e) => {
     // console.log(useState);
-    // setTotal(total + 1);
+    setTotal((prevCount) => prevCount + 1);
 
     console.log(e);
   };
 
   const minusFn = () => {
-    setTotal(total - 1);
+    setTotal((prevCount) => prevCount - 1);
+
   };
 
   return (
